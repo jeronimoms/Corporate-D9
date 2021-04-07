daf354cbd795017285d56de6120b3ea3c96e0107
Ivan Arias <iarias@bilbomatica.es>
2021-04-07 15:48:52 +0200

    MNCW-67 - New Theme

--- web/sites/default/themes/custom/ncwtheme/js/global.js
+++ web/sites/default/themes/custom/ncwtheme/js/global.js
@@ -33,9 +33,19 @@ jQuery(document).ready(function($) {
 
   // Pager index
   $('.pagination').each(function () {
-    let itemLength = $(this).find('.page-item').length;
-    let lastItemIndex=itemLength - 1;
-    $(this).find('.page-item').eq(lastItemIndex).addClass('last');
+    let itemLast = $(this).find('[title="Go to last page"]').length;
+    if(itemLast>0){
+      $(this).find('[title="Go to last page"]').closest('.page-item').addClass('last');
+    }
+    let itemFirst = $(this).find('[title="Go to first page"]').length;
+    if(itemFirst>0){
+      $(this).find('[title="Go to first page"]').closest('.page-item').addClass('first');
+    }
+
+    let itemPrev = $(this).find('[title="Go to previous page"]').length;
+    if(itemPrev>0){
+      $(this).find('[title="Go to previous page"]').closest('.page-item').addClass('prev');
+    }
   });
 
 });
