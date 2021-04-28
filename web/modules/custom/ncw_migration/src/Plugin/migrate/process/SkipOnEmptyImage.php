17288d8333b960397f878b96c4b6f246adce2767
David Biescas <dbiescas@bilbomatica.es>
2021-04-28 14:49:57 +0000

    NCW Image for news

--- web/modules/custom/ncw_migration/src/Plugin/migrate/process/SkipOnEmptyImage.php
+++ web/modules/custom/ncw_migration/src/Plugin/migrate/process/SkipOnEmptyImage.php
@@ -10,25 +10,19 @@ use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
 
 /**
- * Skip on empty, including multivalue fields.
- *
- * This is needed for text fields and many fields on Drupal sources, because
- * even if the cardinality is set to 1, the field data is still available in a
- * single-item array.
+ * Skip on empty images.
  *
  * @MigrateProcessPlugin(
- *   id = "skip_on_empty_multiple",
- *   handle_multiples = TRUE
+ *   id = "skip_on_empty_image",
  * )
  */
-class SkipOnEmptyMultiple extends ProcessPluginBase {
+class SkipOnEmptyImage extends ProcessPluginBase {
 
   /**
    * {@inheritdoc}
    */
   public function process($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     if (!is_array($value)) {
-      ksm($value);
       if (empty($value)) {
         throw new MigrateSkipProcessException();
       }
