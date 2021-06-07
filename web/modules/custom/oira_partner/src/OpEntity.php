173f8858331b7b4e3a7b46f7333f200b29ad62e5
wodby <wodby@example.com>
2021-06-07 09:09:11 +0000

    Oira partner custom preview + unhidde co and 3er partner

--- web/modules/custom/oira_partner/src/OpEntity.php
+++ web/modules/custom/oira_partner/src/OpEntity.php
@@ -5,6 +5,7 @@ namespace Drupal\oira_partner;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\node\Entity\Node;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Entity\EntityInterface;
@@ -15,6 +16,8 @@ use Drupal\oira_partner\OpEntityUpdateManager;
  * General class for entity hooks.
  */
 class OpEntity implements ContainerInjectionInterface {
+
+  use StringTranslationTrait;
 
   /**
    * The Oria entity update manager
@@ -77,20 +80,12 @@ class OpEntity implements ContainerInjectionInterface {
     // Store the fields if the user is parner.
     if (in_array('partner', $this->account->getRoles())) {
       if ($entity instanceof Node) {
-        foreach ($this->fields as $key => $field_name) {
-          // Ignore if the entity doesn't have the one of fields.
-          if ($entity->hasField($key)) {
-            $entity->set($key, $this->opEntityManager->getTermParent());
-          }
-          if ($entity->hasField($field_name)) {
-            $this->opEntityManager->updatePartners($entity, FALSE, FALSE);
-          }
-        }
+        $entity->set('field_workbench_access', $this->opEntityManager->getTermParent());
       }
       return;
     }
 
-    // Store the field from normal edito form.
+    // Store the field from normal edit form.
     $this->opEntityManager->updatePartners($entity, FALSE, FALSE);
   }
 
@@ -126,10 +121,33 @@ class OpEntity implements ContainerInjectionInterface {
           // If the user is a partner, hidde the fields too.
           if (in_array('partner', $this->account->getRoles())) {
             // Hidde the field.
-            $form[$key]['#attributes']['class'][] = 'hidden';
+            $form['field_workbench_access']['#attributes']['class'][] = 'hidden';
+            $form['actions']['submit']['#value'] = $this->t(' Save and preview this new item');
+            $form['actions']['save_final'] = $form['actions']['submit'];
+            $form['actions']['save_final']['#value'] = $this->t('Submit this new item to validation');
+            $form['actions']['save_final']['#weight'] = 999;
+            $form['actions']['save_final']['#submit'][] = [$this, 'omSubmit'];
           }
         }
       }
+    }
+  }
+
+  /**
+   * Adds a new Oira Masquerade submit.
+   *
+   * @param array $form
+   *   The form array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state object
+   */
+  public function omSubmit(array &$form, FormStateInterface $form_state) {
+    /** @var \Drupal\node\Entity\Node $entity */
+    $entity = $form_state->getFormObject()->getEntity();
+    if ($entity) {
+      // Save the node as final_draft.
+      $entity->set('moderation_state', 'final_draft');
+      $entity->save();
     }
   }
 
