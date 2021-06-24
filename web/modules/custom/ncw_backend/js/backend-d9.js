e24e4c14612cc40de6e05bb05a69259ff277e5ae
Ivan Arias <iarias@bilbomatica.es>
2021-06-24 15:04:12 +0200

    Events admin view colours

--- web/modules/custom/ncw_backend/js/backend-d9.js
+++ web/modules/custom/ncw_backend/js/backend-d9.js
@@ -1,12 +1,12 @@
 jQuery(document).ready(function($) {
   $('tr').each(function(){
-    if($('td:contains("Red")', this).length){
+    if($('td:nth-child(1):contains("Red")', this).length){
       $(this).addClass('red');
     }
-    if($('td:contains("Black")', this).length){
+    if($('td:nth-child(1):contains("Black")', this).length){
       $(this).addClass('black');
     }
-    if($('td:contains("Grey")', this).length){
+    if($('td:nth-child(1):contains("Grey")', this).length){
       $(this).addClass('grey');
     }
   });
