e89aaf170067854ceda353d44f057bf521ea5695
David Biescas <dbiescas@bilbomatica.es>
2021-06-30 10:41:00 +0000

    Napo films

---
+++ web/modules/custom/napo_film/src/Controller/NfModalController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Drupal\napo_film\Controller;
+
+use Drupal\Core\Ajax\InsertCommand;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormBuilder;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\TempStore\PrivateTempStore;
+use Drupal\Core\TempStore\PrivateTempStoreFactory;
+use Drupal\Core\Url;
+use Drupal\napo_content_cart\NccCartTrait;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\HtmlCommand;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Ajax\OpenDialogCommand;
+use Drupal\Core\Ajax\ReplaceCommand;
+use Drupal\Core\Ajax\RemoveCommand;
+use Drupal\napo_content_cart\Form\NccDownloadCentreForm;
+
+/**
+ * General class for node like controller.
+ */
+class NfModalController extends ControllerBase implements ContainerInjectionInterface {
+
+  use NccCartTrait;
+
+  /**
+   * The Request object.
+   *
+   * @var \Symfony\Component\HttpFoundation\Request
+   */
+  protected $request;
+
+  /**
+   * The private tem store object.
+   * @var \Drupal\Core\TempStore\PrivateTempStoreFactory
+   */
+  protected $privateTempStoreFactory;
+
+  /**
+   * The form builder manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(RequestStack $request_stack, PrivateTempStoreFactory $privateTempStoreFactory, FormBuilder $form_builder, EntityTypeManagerInterface $entity_type_manager) {
+    $this->request = $request_stack->getCurrentRequest();
+    $this->privateTempStoreFactory = $privateTempStoreFactory;
+    $this->formBuilder = $form_builder;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('request_stack'),
+      $container->get('tempstore.private'),
+      $container->get('form_builder'),
+      $container->get('entity_type.manager'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function show(Node $node) {
+
+
+    $render_controller = \Drupal::entityTypeManager()->getViewBuilder($node->getEntityTypeId());
+    $render_output = $render_controller->view($node, 'video_modal');
+
+
+    return $render_output;
+  }
+
+
+}
