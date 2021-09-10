2533f574632595218ffcb880d6f85a8a96e02c7c
mcacheiro <mcacheiro@bilbomatica.es>
2021-09-10 10:56:57 +0200

    Adding first version of the OSHWiki import code

---
+++ web/modules/custom/oshwiki_import/src/OshWikiNodeEntity.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\oshwiki_import;
+
+class OshWikiNodeEntity
+{
+  public $title;
+  public $content;
+  public $summary;
+  public $modificationDate;
+  public $category = [];
+  public $language = [];
+  public $np = [];
+  public $op = [];
+  public $tags = [];
+}
