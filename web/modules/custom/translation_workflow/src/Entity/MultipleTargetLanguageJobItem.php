751390e1329ddafa7c40e48504411892ad946b06
adelbarrio <adelbarrio@bilbomatica.es>
2021-09-14 23:23:01 +0200

    Add job priority

--- web/modules/custom/translation_workflow/src/Entity/MultipleTargetLanguageJobItem.php
+++ web/modules/custom/translation_workflow/src/Entity/MultipleTargetLanguageJobItem.php
@@ -4,7 +4,6 @@ namespace Drupal\translation_workflow\Entity;
 
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Field\BaseFieldDefinition;
-use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\tmgmt\Entity\JobItem;
 use Drupal\tmgmt\JobItemInterface;
 
