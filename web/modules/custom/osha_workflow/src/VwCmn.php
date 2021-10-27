a24753339636e746997e3338050ea734e41fd298
Maria Tarifa <mtarifa@bilbomatica.es>
2021-10-27 15:36:49 +0200

    dynamic lists

--- web/modules/custom/osha_workflow/src/VwCmn.php
+++ web/modules/custom/osha_workflow/src/VwCmn.php
@@ -96,6 +96,7 @@ class VwCmn implements ContainerInjectionInterface {
    */
   public function mailDataAlter(EntityInterface $entity, array &$data) {
     // Workflow settings of current node.
+    $this->filterUsers($entity, $data);
     $workflow = $this->helper->getWorkflow();
     if (!$workflow){
       $workflow = $entity;
@@ -121,23 +122,34 @@ class VwCmn implements ContainerInjectionInterface {
       $users = $this->helper->getModerationList($table);
       $users = (array) $users;
       /** @var \Drupal\user\Entity\User $user */
-      $user = $this->entityTypeManager->getStorage('user')->load($users[0]->user_id);
-      $data['to'] = [
-        0 => $user->getEmail(),
-      ];
+        unset($data['to']);
+        $data['to'] = [];
+      for ($i=0; $i< count($users); $i++ ) {
+        $user = $this->entityTypeManager->getStorage('user')->load($users[$i]->user_id);
+        if($user->isActive() && $user->hasRole('review_manager')){
+          array_push($data['to'], $user->getEmail());
+        }
+      }
       $this->helper->resetUsersStatus('reviewers');
     }
 
-    // Alter the notification to send the email to the next user of.
-    // list if exists.
-    if ($notification->getOriginalId() == 'draft_to_final_draft') {
-      $next_user = $this->helper->getNextUser($table);
-      if (!empty($next_user)) {
-        $data['to'] = [
-          0 => $next_user->getEmail(),
-        ];
+    // Alter the notification to filter users.
+    if ($notification->getOriginalId() == 'to_approved_to_be_reviewed') {
+      $users = $this->helper->getModerationList($table);
+      $users = (array) $users;
+      /** @var \Drupal\user\Entity\User $user */
+      unset($data['to']);
+      $data['to'] = [];
+      for ($i=0; $i< count($users); $i++ ) {
+        $user = $this->entityTypeManager->getStorage('user')->load($users[$i]->user_id);
+        if($user->isActive() && $user->hasRole('project_manager')){
+          ksm($user);
+          array_push($data['to'], $user->getEmail());
+        }
       }
+      $this->helper->resetUsersStatus('project_managers');
     }
+
 
     // Alter the notification to send the email only to the first user.
     if ($notification->getOriginalId() == 'from_any_status_to_to_be_approved') {
@@ -150,11 +162,12 @@ class VwCmn implements ContainerInjectionInterface {
       ];
 
       $this->helper->resetUsersStatus('approvers');
+
     }
 
     // Alter the notification to send the email to the next user of.
     // list if exists.
-    if ($notification->getOriginalId() == 'from_any_status_to_to_be_approved') {
+    if ($notification->getOriginalId() == 'to_next_approver') {
       $next_user = $this->helper->getNextUser($table);
       if (!empty($next_user)) {
         $data['to'] = [
@@ -162,6 +175,80 @@ class VwCmn implements ContainerInjectionInterface {
         ];
       }
     }
+  }
+
+  public function filterUsers(EntityInterface $entity, array &$data){
+
+      // Get the section of the node.
+      if($entity->hasField('field_section') && $entity->get('field_section')->get(0) != null){
+        $nodeSection = $entity->get('field_section')->get(0)->getValue();
+      }
+      else{
+        $nodeSection = null;
+      }
+
+      if( !$nodeSection || is_null($nodeSection) || empty($nodeSection) ){
+        $data['to'] = [];
+        return \Drupal::service('class_resolver')
+          ->getInstanceFromDefinition(VwCmn::class)
+          ->mailDataAlter($entity, $data);
+      }
+
+      $nodeSectionId = $nodeSection['target_id'];
+      if(!$nodeSectionId || empty($nodeSectionId)){
+        $data['to'] = [];
+        return \Drupal::service('class_resolver')
+          ->getInstanceFromDefinition(VwCmn::class)
+          ->mailDataAlter($entity, $data);
+      }
+
+      // Get the section ids with the corresponding section of the node.
+      $querySaResult = \Drupal::database()
+        ->query('SELECT e.id
+            FROM section_association e
+            WHERE e.section_id = :section_id', array(
+          ':section_id' => $nodeSectionId,
+        ));
+      $entityId = "";
+      foreach ($querySaResult as $item) {
+        $entityId= $item->id;
+      }
+
+      // Get the user ids from the section_association__user_id with the corresponding section id.
+      $queryUserSectionResult = \Drupal::database()
+        ->query('SELECT sa.user_id_target_id
+            FROM section_association__user_id sa
+            WHERE sa.entity_id = :entity_id', array(
+          ":entity_id" => $entityId,
+        ));
+      $sectionUserTargetIds = [];
+      foreach ($queryUserSectionResult as $item) {
+        array_push($sectionUserTargetIds,$item->user_id_target_id);
+      }
+
+      // Get the users from mail from data['to'].
+      $data_ids =[];
+      foreach ($data['to'] as $receivedMail) {
+        if(user_load_by_mail($receivedMail) instanceof \Drupal\ds\Plugin\DsField\User\User){
+          array_push($data_ids);
+        }
+      }
+
+      // Compare users from data['to']  with  users that belong to the section of the node.
+      $sectionBelongingUsers = array_diff($sectionUserTargetIds,$data_ids);
+   //   $allowedRoles = ['project_manager'];
+
+      // Set data['to'] to the section Belonging users
+      unset($data['to']);
+      $data['to'] = [];
+      foreach ($sectionBelongingUsers as $item) {
+        $theUser = \Drupal::entityTypeManager()->getStorage('user')->load($item);
+        if($theUser->isActive()){
+        //  if(count(array_intersect(($theUser->getRoles()), $allowedRoles)) > 0){
+            array_push($data['to'], $theUser->mail->value);
+       //   }
+        }
+      }
   }
 
 }
