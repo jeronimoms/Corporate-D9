e201d569d54655e22505d7d6beab409e7587c659
jmartinez.salas <jmartinez.salas@bilbomatica.es>
2021-11-24 19:15:13 +0100

    form fields results and results_final were wrongly managed in scanner, so override submitForm in SarScannerConfirmForm

--- web/modules/custom/search_and_replace/src/Form/SarScannerConfirmForm.php
+++ web/modules/custom/search_and_replace/src/Form/SarScannerConfirmForm.php
@@ -2,8 +2,12 @@
 
 namespace Drupal\search_and_replace\Form;
 
+use Drupal\Component\Plugin\Exception\PluginException;
+use Drupal\Core\Form\ConfirmFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 use Drupal\Core\TempStore\PrivateTempStoreFactory;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\scanner\Form\ScannerConfirmForm;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -12,6 +16,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class SarScannerConfirmForm extends ScannerConfirmForm {
 
+  use StringTranslationTrait;
   /**
    * The Private temporary storage factory.
    *
@@ -41,7 +46,6 @@ class SarScannerConfirmForm extends ScannerConfirmForm {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $build = parent::buildForm($form, $form_state);
-
     $scannerStore = $this->tempStore->get('scanner');
     $operations = [
       'search',
@@ -53,11 +57,55 @@ class SarScannerConfirmForm extends ScannerConfirmForm {
       'followed',
       'published',
       'language',
+      'results',
+      'results_final',
     ];
     foreach ($operations as $value) {
       $values[$value] = $scannerStore->get($value);
     }
     return $build;
   }
-
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $scannerStore = $this->tempStore->get('scanner');
+    foreach ([
+      'search',
+      'replace',
+      'mode',
+      'wholeword',
+      'regex',
+      'preceded',
+      'followed',
+      'published',
+      'language',
+      'results',
+      'results_final',
+    ] as $value) {
+      $values[$value] = $scannerStore->get($value);
+    }
+    $fields = \Drupal::config('scanner.admin_settings')->get('fields_of_selected_content_type');
+    $operations = [];
+    // Build an array of batch operation jobs. Batch job will need the field
+    // and the filter values the users entered in the form.
+    foreach ($fields as $field) {
+      $operations[] = [
+        '\Drupal\scanner\Form\ScannerConfirmForm::batchReplace',
+        [
+          $field,
+          $values,
+        ],
+      ];
+    }
+    $batch = [
+      'title' => $this->t('Scanner Replace Batch'),
+      'operations' => $operations,
+      'finished' => '\Drupal\scanner\Form\ScannerConfirmForm::batchFinished',
+      'progress_message' => $this->t('Processed @current out of @total'),
+    ];
+    batch_set($batch);
+    // Redirect to the scanner page after the batch is done.
+    $form_state->setRedirect('scanner.admin_content');
+  }
 }
